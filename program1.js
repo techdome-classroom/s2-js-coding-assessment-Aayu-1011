9d27928ebdbe8cf2426cb8c2c4bf8187d734c687
aayushicollegites <aayushi@collegites.tech>
2024-09-30 14:03:32 +0530

    program1 all test cases passed

--- program1.js
+++ program1.js
@@ -3,7 +3,26 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    let stack=[];
+    let mapping={
+        ')':'(',
+        '}':'{',
+        ']':'['
+    };
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+
+        if (char in mapping) {
+            let topElement = stack.length ? stack.pop() : '#';
+
+            if (mapping[char] !== topElement) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+    return stack.length==0;
 };
 
 module.exports = { isValid };
